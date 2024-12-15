c75ff32fa610cd031dff5fdb272cb012226e71ac
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2024-12-14 22:14:23 -0600

    Fix fo.py

--- scripts/fo.py
+++ scripts/fo.py
@@ -2,5 +2,5 @@ import fiftyone as fo
 
 if __name__ == "__main__":
     # Ensures that the App processes are safely launched on Windows
-    session = fo.launch_app(dataset)
+    session = fo.launch_app(port=5151)
     session.wait()
