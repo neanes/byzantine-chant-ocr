0f9d6a0d0fa9b298b385782db976be7f43e878f9
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2024-12-24 12:01:06 -0600

    Clean-up

--- scripts/segment_test.py
+++ scripts/segment_test.py
@@ -5,20 +5,35 @@ sys.path.append("../src")
 from segmentation import segment
 from text_removal import remove_text
 
-img_path = "../data/__pages/vespers_sam_p0042.png"
-# img_path = "../data/__pages/vespers_sam_p0035.png"
-# img_path = "../data/__pages/heirmologion_john_p0005.png"
 
-img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+def test_segmentation(img_path, output_path):
+    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
-binary = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    binary = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 
-segmentation = segment(binary)
+    segmentation = segment(binary)
+
+    print(segmentation)
+
+    no_text = remove_text(binary, segmentation)
+    cv2.imwrite(output_path, no_text)
+
+    cv2.namedWindow("with text removed", cv2.WINDOW_NORMAL)
+    cv2.imshow("with text removed", no_text)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
 
-print(segmentation)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please specify an image file path.")
+        exit(1)
 
-cv2.namedWindow("with text removed", cv2.WINDOW_NORMAL)
-cv2.imshow("with text removed", remove_text(binary, segmentation))
-cv2.waitKey()
-cv2.destroyAllWindows()
+    filepath = sys.argv[1]
+
+    output_filepath = "output.png"
+
+    if len(sys.argv) > 3:
+        output_filepath = sys.argv[2]
+
+    test_segmentation(filepath, output_filepath)
